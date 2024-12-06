f7db674e6a6986a12acacbabb00ab056ff56fab2
EL-J Diaz <91679071+EJD222@users.noreply.github.com>
2024-12-06 13:48:01 +0800

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Force HTTPS if the environment is not local
+        if (config('app.env') !== 'local') {
+            \URL::forceScheme('https');
+        }
     }
 }
